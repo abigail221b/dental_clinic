c31380dd50d82911b0de83f5a151689cf57183e5
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-17 09:50:53 -0700

    Implement UpdateDentistForm page

--- dentalclinic_frontend/src/Dentists/UpdateDentistForm.js
+++ dentalclinic_frontend/src/Dentists/UpdateDentistForm.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const UpdateDentistForm = () => {
-    return "Update Dentist Form";
+
+    const [dentist, setDentist] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/dentist/${ id }`)
+        .then(res => res.json())
+        .then(dentist => setDentist(dentist));
+    }, [id]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:8080/dentist", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dentist)
+        });
+    }
+
+    return (
+        <form>
+            <input type="text" value={dentist.firstName} onChange={(e) => setDentist(dentist => { return {...dentist, firstName: e.target.value }})} />
+            <input type="text" value={dentist.lastName} onChange={(e) => setDentist(dentist => {return {...dentist, lastName: e.target.value }})}/>
+            <input type="text" value={dentist.phoneNumber} onChange={(e) => setDentist(dentist => {return {...dentist, phoneNumber: e.target.value }})}/>
+            <button onClick={ (e) => handleSubmit(e) }>Update</button>
+        </form>
+    );
 }
 
 export default UpdateDentistForm;
