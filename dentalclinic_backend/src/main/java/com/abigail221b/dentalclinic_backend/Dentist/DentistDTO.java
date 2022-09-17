ea411d5bccee4c8ee0f745af146cd731319ab0e6
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 19:17:03 -0700

    Initialize Dentist DTO

---
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Dentist/DentistDTO.java
@@ -0,0 +1,11 @@
+package com.abigail221b.dentalclinic_backend.Dentist;
+
+import lombok.Data;
+
+@Data
+public class DentistDTO {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
