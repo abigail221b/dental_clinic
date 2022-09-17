0907f696e12bb639d51ae4fbf73dda393e67fdb3
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 17:22:27 -0700

    Fetch and render patients from the database

--- dentalclinic_frontend/src/Patients/PatientsList.js
+++ dentalclinic_frontend/src/Patients/PatientsList.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
 const PatientsList = () => {
-    return "Patients List";
+
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/patient")
+        .then(res => res.json())
+        .then(patients => setPatients(patients));
+    }, []);
+
+    return (
+        <>
+            { patients.map(patient => <p> {patient.id} 
+                                            {patient.firstName} 
+                                            {patient.lastName} 
+                                            {patient.dateOfBirth} 
+                                            {patient.address} 
+                                            {patient.phoneNumber}</p>) }
+        </>
+    );
 }
 
 export default PatientsList;
