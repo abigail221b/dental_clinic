e8200d92863bd170bc43ef7b2f9eae78ca356b08
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-17 09:53:49 -0700

    Create component to render Dentist info

--- dentalclinic_frontend/src/Dentists/DentistRow.js
+++ dentalclinic_frontend/src/Dentists/DentistRow.js
@@ -1,5 +1,11 @@
-const DentistRow = () => {
-    return "Dentist row";
+import { Link } from "react-router-dom";
+
+const DentistRow = ({id, firstName, lastName, phoneNumber}) => {
+    return (
+        <div>
+            <span>{id} {firstName} {lastName} {phoneNumber}</span>
+        </div>
+    );
 }
 
 export default DentistRow;
