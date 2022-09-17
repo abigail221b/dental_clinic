b0fd681b80048f4ae0c440b67637be5756016f1e
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 17:13:11 -0700

    Add a Link and Outlet to the patient route

--- dentalclinic_frontend/src/App.js
+++ dentalclinic_frontend/src/App.js
@@ -1,7 +1,14 @@
+import { Link, Outlet } from "react-router-dom";
 import './App.css';
 
 function App() {
-	return "App";
+	return (
+		<>
+			<Link to="patient">Patients</Link>
+			<hr />
+			<Outlet />
+		</>
+	);
 }
 
 export default App;
