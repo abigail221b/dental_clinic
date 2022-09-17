2276db6315adb56b158ce0cbec6f208dca07ea33
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-17 09:57:30 -0700

    Create DentistList page to render DentistRows

--- dentalclinic_frontend/src/Dentists/DentistList.js
+++ dentalclinic_frontend/src/Dentists/DentistList.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import DentistRow from "./DentistRow";
+
 const DentistList = () => {
-    return "Dentist list";
+
+    const [dentists, setDentists] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/dentist")
+        .then(res => res.json())
+        .then(dentists => setDentists(dentists));
+    }, []);
+
+    return (
+        <>
+            { dentists.map(dentist => <DentistRow id={dentist.id} 
+                                                  firstName={dentist.firstName}
+                                                  lastName={dentist.lastName}
+                                                  phoneNumber={dentist.phoneNumber} />)}
+        </>
+    );
 }
 
 export default DentistList;
