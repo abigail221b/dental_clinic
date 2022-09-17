25f5b9fca40d131ad28d78f6fd2be41e8ca94e89
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 18:30:30 -0700

    Add routes to PatientDetail and UpdatePatientForm

--- dentalclinic_frontend/src/index.js
+++ dentalclinic_frontend/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PatientsDashboard from './Patients/PatientsDashboard';
 import PatientsList from './Patients/PatientsList';
 import NewPatientForm from './Patients/NewPatientForm';
+import PatientDetail from './Patients/PatientDetail';
+import UpdatePatientForm from './Patients/UpdatePatientForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +18,8 @@ root.render(
 					<Route path="patient" element={ <PatientsDashboard />}>
 						<Route path="all" element={ <PatientsList /> }/>
 						<Route path="new" element={ <NewPatientForm />}/>
+						<Route path=":id" element={ <PatientDetail /> } />
+						<Route path="update/:id" element={ <UpdatePatientForm /> } />
 					</Route>
 				</Route>
 			</Routes>
