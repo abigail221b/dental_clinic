8aac98b188ec4c86d4467d236d35292a5e65f06e
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 19:16:54 -0700

    Initialize DentistRepository

---
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Dentist/DentistRepository.java
@@ -0,0 +1,7 @@
+package com.abigail221b.dentalclinic_backend.Dentist;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DentistRepository extends CrudRepository<Dentist, Integer> {
+    
+}
