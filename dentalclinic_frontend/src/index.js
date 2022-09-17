f345288982c7df7cb5a77340c7feeaefbe80e973
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 17:10:36 -0700

    Setup routes for App and Patient related pages

--- dentalclinic_frontend/src/index.js
+++ dentalclinic_frontend/src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PatientsDashboard from './Patients/PatientsDashboard';
+import PatientsList from './Patients/PatientsList';
+import NewPatientForm from './Patients/NewPatientForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={ <App /> }>
+					<Route path="patient" element={ <PatientsDashboard />}>
+						<Route path="all" element={ <PatientsList /> }/>
+						<Route path="new" element={ <NewPatientForm />}/>
+					</Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
