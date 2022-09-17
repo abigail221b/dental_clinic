b0a94eb46bd73b0f9768d10458015b29a43898e0
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-17 09:55:40 -0700

    Create NewDentistForm page

--- dentalclinic_frontend/src/Dentists/NewDentistForm.js
+++ dentalclinic_frontend/src/Dentists/NewDentistForm.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const NewDentistForm = () => {
-    return "New Dentist Form";
+
+    const [dentist, setDentist] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:8080/dentist", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dentist)
+        });
+    }
+
+    return (
+        <form>
+            <input type="text" onChange={(e) => setDentist(dentist => { return {...dentist, firstName: e.target.value }})} />
+            <input type="text" onChange={(e) => setDentist(dentist => {return {...dentist, lastName: e.target.value }})}/>
+            <input type="text" onChange={(e) => setDentist(dentist => {return {...dentist, phoneNumber: e.target.value }})}/>
+            <button onClick={ (e) => handleSubmit(e) }>Submit</button>
+        </form>
+    );
 }
 
 export default NewDentistForm;
