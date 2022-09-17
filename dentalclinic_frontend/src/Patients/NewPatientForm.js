7ad38610f66ff8a9205605e742be97d7f928a3b4
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-16 17:36:19 -0700

    Render form and handle patient POST submission

--- dentalclinic_frontend/src/Patients/NewPatientForm.js
+++ dentalclinic_frontend/src/Patients/NewPatientForm.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const NewPatientForm = () => {
-    return "New Patient Form";
+
+    const [patient, setPatient] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        fetch("http://localhost:8080/patient", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(patient)
+        });
+    }
+
+    return (
+        <form>
+            <input type="text" placeholder="First Name"   onChange={(e) => setPatient(patient => { return {...patient, firstName: e.target.value }}) } />
+            <input type="text" placeholder="Last Name"    onChange={(e) => setPatient(patient => { return {...patient, lastName: e.target.value }}) } />
+            <input type="date"                            onChange={(e) => setPatient(patient => { return {...patient, dateOfBirth: e.target.value }}) } />
+            <input type="text" placeholder="Address"      onChange={(e) => setPatient(patient => { return {...patient, address: e.target.value }}) } />
+            <input type="text" placeholder="Phone Number" onChange={(e) => setPatient(patient => { return {...patient, phoneNumber: e.target.value }}) } />
+            <button onClick={ (e) => handleSubmit(e) }>Submit</button>
+        </form>
+    );
 }
 
 export default NewPatientForm;
