64eec61632567a19a9af43fe6cae4b190da8707e
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-17 09:50:20 -0700

    Fetch Dentist detail by id then display

--- dentalclinic_frontend/src/Dentists/DentistDetail.js
+++ dentalclinic_frontend/src/Dentists/DentistDetail.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
 const DentistDetail = () => {
-    return "Dentist detail";
+
+    const [dentist, setDentist] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/dentist/${ id }`)
+        .then(res => res.json())
+        .then(dentist => setDentist(dentist));
+    }, []);
+
+    return (
+        <>
+            <span>{dentist.id} {dentist.firstName} {dentist.lastName} {dentist.phoneNumber}</span>
+        </>
+    );
 }
 
 export default DentistDetail;
