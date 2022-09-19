30ba03b20e29f133fa3fdc4aeb3d83ddb21c4293
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 14:50:22 -0700

    Map Appointment to Dentist

--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/Appointment.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/Appointment.java
@@ -2,6 +2,10 @@ package com.abigail221b.dentalclinic_backend.Appointment;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.abigail221b.dentalclinic_backend.Dentist.Dentist;
 
 import lombok.Data;
 
@@ -11,7 +15,9 @@ public class Appointment {
     @EmbeddedId
     private AppointmentID id;
 
-    private int dentistID;
+    @ManyToOne
+    @JoinColumn(name = "dentistID")
+    private Dentist dentist;
     private int duration;
     private String description;
 
