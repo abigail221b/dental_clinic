bcfa9c23f04076dafc83ee3eb183e2a5af227c6a
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 13:33:01 -0700

    Remove Link and Outlet

--- dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
+++ dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
@@ -1,10 +1,8 @@
-import { Link, Outlet } from "react-router-dom";
 
 const AppointmentDashboard = () => {
     return (
         <>
-            <Link to="new">Add Appointment</Link>
-            <Outlet />
+            
         </>
     );
 }
