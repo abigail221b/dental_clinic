33933204bfaa5b612975b3128e774c1bb776e0bd
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 14:50:37 -0700

    Map Appointment to Patient

--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentID.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentID.java
@@ -5,13 +5,20 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.abigail221b.dentalclinic_backend.Patient.Patient;
 
 import lombok.Data;
 
 @Embeddable
 @Data
 public class AppointmentID implements Serializable {
-    private int patientID;
+    @ManyToOne
+    @JoinColumn(name = "patientID")
+    private Patient patient;
+    
     private LocalDate date;
     private LocalTime startTime;
 }
