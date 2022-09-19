dbc6b02fcd08f81b64ca7abfdc58010eb2d3a3f7
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 13:53:59 -0700

    Add date input

--- dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
+++ dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
 
 const AppointmentDashboard = () => {
+
+    const [date, setDate] = useState(new Date()
+                                        .toLocaleDateString("en-CA", {
+                                                        year: "numeric",
+                                                        month: "numeric",
+                                                        day: "numeric"
+                                                    }));
+
     return (
         <>
-            
+            <input type="date" 
+                   onChange={(e) => setDate(e.target.value) }
+                   value={ date }/>
         </>
     );
 }
