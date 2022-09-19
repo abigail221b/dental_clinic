b04ea62b6fb164dfa574e4774eea90b9c79a5900
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 13:55:27 -0700

    Add Dentist select input Keep track of selected dentist

--- dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
+++ dentalclinic_frontend/src/Appointments/AppointmentDashboard.js
@@ -8,12 +8,27 @@ const AppointmentDashboard = () => {
                                                         month: "numeric",
                                                         day: "numeric"
                                                     }));
+    const [dentists, setDentists] = useState([]);
+    const [selectedDentistID, setSelectedDentistID] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/dentist")
+        .then(res => res.json())
+        .then(dentists => {
+            setDentists(dentists);
+            setSelectedDentistID(dentists[0].id);
+        });
+    }, []);
 
     return (
         <>
             <input type="date" 
                    onChange={(e) => setDate(e.target.value) }
                    value={ date }/>
+            
+            <select value={selectedDentistID} onChange={(e) => setSelectedDentistID(e.target.value)}>
+                { dentists.map(dentist => <option value={ dentist.id }> Dr. {dentist.firstName} {dentist.lastName}</option>) }
+            </select>
         </>
     );
 }
