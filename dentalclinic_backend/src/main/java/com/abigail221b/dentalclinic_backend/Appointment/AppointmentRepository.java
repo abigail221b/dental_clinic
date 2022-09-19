73c2f3ccced0dc5825bd1a830a5abd9abd354984
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-19 14:51:46 -0700

    Add method query

--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentRepository.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentRepository.java
@@ -1,7 +1,10 @@
 package com.abigail221b.dentalclinic_backend.Appointment;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, AppointmentID> {
-    
+    public List<Appointment> findAllById_DateAndDentistId(LocalDate date, int dentistID);
 }
