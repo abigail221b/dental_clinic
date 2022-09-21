a4de1f96f36f2d709df15e870e71ee58221a8c16
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-20 19:07:31 -0700

    Add input controls for the search form

--- dentalclinic_frontend/src/Patients/SearchPatientForm.js
+++ dentalclinic_frontend/src/Patients/SearchPatientForm.js
@@ -1,5 +1,13 @@
 const SearchPatientForm = () => {
-    return "Search Patient Form";
+    return (
+        <form>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="date" />
+            <input type="text" placeholder="Phone Number" />
+            <button>Search</button>
+        </form>
+    );
 }
 
 export default SearchPatientForm;
