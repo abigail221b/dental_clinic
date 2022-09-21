81012232edaec1605e46c8e5a45a4c44f33e2ab8
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-21 16:40:58 -0700

    Fetch and display a Patients upcoming appointments

--- dentalclinic_frontend/src/Patients/PatientDetail.js
+++ dentalclinic_frontend/src/Patients/PatientDetail.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 const PatientDetail = () => {
     const [patient, setPatient] = useState({});
     const { id } = useParams();
+    const [upcomingAppointments, setUpcomingAppointments] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:8080/patient/${ id }`)
@@ -11,11 +12,24 @@ const PatientDetail = () => {
         .then(patient => setPatient(patient));
     }, [id]);
 
+    useEffect(() => {
+        fetch(`http://localhost:8080/appointment/?patientID=${ id }&after=${ new Date().toLocaleDateString("en-CA", {year: "numeric", month: "numeric", day: "numeric"}) }`)
+        .then(res => res.json())
+        .then(upcomingAppointments => setUpcomingAppointments(upcomingAppointments));
+    }, []);
+
     return (
         <>
             <span>{patient.id} {patient.firstName} {patient.lastName} {patient.dateOfBirth} {patient.address} {patient.phoneNumber}</span>
             <Link to={{ pathname: `/patient/${ id }/update-form`}}> Update </Link>
             <Link to={{ pathname: `/patient/${ id }/appointment-form`}}> Book Appointment </Link>
+
+            <h2>Upcoming Appointments</h2>
+            { upcomingAppointments.map(appointment => <p>{appointment.id.patient.firstName} 
+                                                         {appointment.id.patient.lastName}  
+                                                         {appointment.id.date} 
+                                                         {appointment.id.startTime} 
+                                                         Dr. {appointment.dentist.lastName}</p>) }
         </>
     );
 }
