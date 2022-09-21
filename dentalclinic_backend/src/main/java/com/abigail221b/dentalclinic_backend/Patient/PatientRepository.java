2a739048eada0caaca24ff08ea920b1ac930898b
Abigail Ponce <abigaiil221b@gmail.com>
2022-09-20 20:12:41 -0700

    Add custom query Finds patients based on search parameters

--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Patient/PatientRepository.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Patient/PatientRepository.java
@@ -1,7 +1,15 @@
 package com.abigail221b.dentalclinic_backend.Patient;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PatientRepository extends CrudRepository<Patient, Integer> {
-    
+
+    @Query(
+        value = "SELECT * FROM patient p WHERE (:firstName is null OR p.first_name=:firstName) AND (:lastName is null OR p.last_name=:lastName) AND (:dateOfBirth is null OR p.date_of_birth=:dateOfBirth) AND (:phoneNumber is null OR p.phone_number=:phoneNumber)",
+        nativeQuery = true
+    )
+    List<Patient> findBySearchParams(String firstName, String lastName, String dateOfBirth, String phoneNumber);
 }
