0abb6031c4e05ac41b56a6d2cb5d7cecce962d18
Abigail Ponce <abigaiil221b@gmail.com>
2022-10-01 07:58:10 -0700

    Wrap ChakraProvider at the root of App

--- dentalclinic_frontend/src/App.js
+++ dentalclinic_frontend/src/App.js
@@ -1,15 +1,16 @@
 import { Link, Outlet } from "react-router-dom";
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 	return (
-		<>
+		<ChakraProvider>
 			<Link to="patient">Patients</Link>
 			<Link to="dentist">Dentist</Link>
 			<Link to="appointment">Appointments</Link>
 			<hr />
 			<Outlet />
-		</>
+		</ChakraProvider>
 	);
 }
 
