943556196a392efc614c82a1cd483f945a91eb7b
Abigail Ponce <abigaiil221b@gmail.com>
2022-10-01 08:39:59 -0700

    Wrap each row with Chakra Tr, Td components

--- dentalclinic_frontend/src/Dentists/DentistRow.js
+++ dentalclinic_frontend/src/Dentists/DentistRow.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { Tr, Td } from '@chakra-ui/react'
 
 const DentistRow = ({id, firstName, lastName, phoneNumber}) => {
     return (
-        <div>
-            <span>{id} {firstName} {lastName} {phoneNumber}</span>
-            <Link to={{ pathname: `/dentist/${id}` }}>View</Link>
-            <Link to={{ pathname: `/dentist/${id}/update-form` }}>Edit</Link>
-        </div>
+        <Tr>
+            <Td>{ firstName }</Td>
+            <Td>{ lastName }</Td>
+            <Td>{ phoneNumber }</Td>
+            <Td>
+                <Link to={{ pathname: `/dentist/${id}` }}>View</Link>
+                <Link to={{ pathname: `/dentist/${id}/update-form` }}>Edit</Link>
+            </Td>
+        </Tr>
     );
 }
 
--- dentalclinic_frontend/src/Patients/PatientRow.js
+++ dentalclinic_frontend/src/Patients/PatientRow.js
@@ -1,13 +1,20 @@
 import { Link, Outlet } from "react-router-dom";
+import { Tr, Td } from '@chakra-ui/react'
 
 const PatientRow = ({ id, firstName, lastName, dateOfBirth, address, phoneNumber}) => {
     return (
-        <div>
-            <span>{id} {firstName} {lastName} {dateOfBirth} {address} {phoneNumber}</span>
-            <Link to={{ pathname: `/patient/${ id }`}}> View </Link>
-            <Link to={{ pathname: `/patient/${ id }/update-form`}}> Update </Link>
-            <Link to={{ pathname: `/patient/${ id }/appointment-form`}}> Book Appointment </Link>
-        </div>
+        <Tr>
+            <Td>{ firstName }</Td>
+            <Td>{ lastName }</Td>
+            <Td>{ dateOfBirth }</Td>
+            <Td>{ address }</Td>
+            <Td>{ phoneNumber }</Td>
+            <Td>
+                <Link to={{ pathname: `/patient/${ id }`}}> View </Link>
+                <Link to={{ pathname: `/patient/${ id }/update-form`}}> Update </Link>
+                <Link to={{ pathname: `/patient/${ id }/appointment-form`}}> Book Appointment </Link>
+                </Td>
+        </Tr>
     );
 }
 
