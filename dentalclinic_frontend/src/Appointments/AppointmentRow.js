04ce19a69588bfef8409df2a0a7270af778c8edc
Abigail Ponce <abigaiil221b@gmail.com>
2022-10-02 19:29:28 -0700

    Handle deleting an appointment from the db and ui

--- dentalclinic_frontend/src/Appointments/AppointmentRow.js
+++ dentalclinic_frontend/src/Appointments/AppointmentRow.js
@@ -2,6 +2,27 @@ import { Tr, Td, Flex, Button } from "@chakra-ui/react";
 
 const AppointmentRow = ({ patientID, patientFN, patientLN, date, startTime, dentistFN, dentistLN, duration, description, setAppointments }) => {
     
+    const handleDelete = () => {
+        fetch(`http://localhost:8080/appointments`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                patientID: patientID,
+                date: date,
+                startTime: startTime
+            })
+        })
+        .then(res => {
+            if(res.status === 200)
+                setAppointments(appointments => appointments.filter(appointment => appointment.id.patient.id !== patientID && appointment.id.date !== date && appointment.id.startTime !== startTime));
+
+            return res.text();
+        })
+        .then(res => console.log(res));
+    }
+    
     return (
         <Tr>
             { patientFN !== null || patientLN !== null? 
@@ -24,7 +45,7 @@ const AppointmentRow = ({ patientID, patientFN, patientLN, date, startTime, dent
             <Td>
                 <Flex gap="5px">
                     <Button colorScheme="teal" size="xs">Update</Button>
-                    <Button colorScheme="teal" size="xs">Cancel</Button>
+                    <Button colorScheme="teal" size="xs" onClick={ handleDelete }>Cancel</Button>
                 </Flex>
             </Td>
         </Tr>
