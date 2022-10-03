98cc28fc01f482fac6d90eeacd3667939560845c
Abigail Ponce <abigaiil221b@gmail.com>
2022-10-02 17:02:19 -0700

    Handle cascading delete of parent entity

--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/Appointment.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/Appointment.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.abigail221b.dentalclinic_backend.Dentist.Dentist;
 
 import lombok.Data;
@@ -16,6 +19,7 @@ public class Appointment {
     private AppointmentID id;
 
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "dentistID")
     private Dentist dentist;
     private int duration;
--- dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentID.java
+++ dentalclinic_backend/src/main/java/com/abigail221b/dentalclinic_backend/Appointment/AppointmentID.java
@@ -8,6 +8,9 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.abigail221b.dentalclinic_backend.Patient.Patient;
 
 import lombok.Data;
@@ -16,6 +19,7 @@ import lombok.Data;
 @Data
 public class AppointmentID implements Serializable {
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "patientID")
     private Patient patient;
     
